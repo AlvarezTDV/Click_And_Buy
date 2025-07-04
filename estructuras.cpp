d557a6808938a45414141369022fe8090fc8ac3c
AlvarezTDV <dalvarezt@unjbg.edu.pe>
2025-07-04 14:30:06 -0500

    Se completaron las clases de los componentes

--- estructuras.cpp
+++ estructuras.cpp
@@ -1,14 +1,13 @@
 #include "ESTRUCTURAS_H"
 
-class componente {
+class componenteYPeriferico {
 	protected:
 		string nombre;
 		string marca;
 		int precio;
-
 };
 
-class gpu : public componente {
+class gpu : public componenteYPeriferico {
 	private:
 		int nucleos;
 		int ventiladores;
@@ -16,10 +15,95 @@ class gpu : public componente {
 		string generacion;
 };
 
-class cpu : public componente {
+class cpu : public componenteYPeriferico {
 	private:
 		int nucleos;
+		int hilos;
 		int frecuencia;
 		string generacion;
 		string socket;
+};
+
+class psu : public componenteYPeriferico {
+	private:
+		int watts;
+		bool tieneCertificacion;
+		string certificacion;
+		bool esModular;
+};
+
+class ram : public componenteYPeriferico {
+	private:
+		string tipo;
+		int capacidad;
+		int velocidad;
+		int latencia;
+};
+
+class motherboard : public componenteYPeriferico {
+	private:
+		string formato;
+		string chipset;
+		string socketCpu;
+		int slotsRam;
+};
+
+class ssd : public componenteYPeriferico {
+	private:
+		int capacidad;
+		int velocidadLectura;
+		int velocidadEscritura;
+		string pcie;
+};
+
+class disipador : public componenteYPeriferico {
+	private:
+		string compatibilidadSocket;
+		int heatPipes;
+};
+
+class refrigeracionLiquida : public componenteYPeriferico {
+	private:
+		string compatibilidadSocket;
+		int numVentiladores;
+};
+
+class gabinete : public componenteYPeriferico {
+	private:
+		string tamanio;
+		string material;
+};
+
+class monitor : public componenteYPeriferico {
+	private:
+		int tamanioPantalla;
+		string resolucion;
+		int tasaRefresco;
+};
+
+class teclado: public componenteYPeriferico {
+	private:
+		string switches;
+		string distribucionTeclas;
+		bool iluminacion;
+};
+
+class mouse : public componenteYPeriferico {
+	private:
+		int dpi;
+		int numBotones;
+		int peso;
+};
+
+class auricular : public componenteYPeriferico {
+	private:
+		int tiempoRespuesta;
+		bool microfono;
+};
+
+class microfono : public componenteYPeriferico {
+	private:
+		int tiempoRespuesta;
+		bool tieneSoporte;
+		bool filtroAntiRuido;
 };
