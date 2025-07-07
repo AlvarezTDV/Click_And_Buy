764569f85e8d2b240689cf3484bdf016627cb610
AlvarezTDV <dalvarezt@unjbg.edu.pe>
2025-07-06 23:24:20 -0500

    Se agrego una funcion para numeros aleatorios

---
+++ funciones.cpp
@@ -0,0 +1,14 @@
+#include "funciones.h"
+
+#include <random>
+using namespace std;
+
+//FUNCIONES QUE SE VAYAN A USAR DE MANERA REPETIDA
+
+//GENERAR NUMEROS ALEATORIOS
+int NumRandom(int min, int max) {
+	static random_device rd;
+	static mt19937 gen(rd());
+	uniform_int_distribution<int> dis(min, max);
+	return dis(gen);
+}
---
+++ funciones.h
@@ -0,0 +1,11 @@
+#ifndef funciones_h
+#define funciones_h
+
+using namespace std;
+
+//FUNCIONES QUE SE VAYAN A USAR DE MANERA REPETIDA
+
+//GENERAR NUMEROS ALEATORIOS
+int NumRandom(int, int);
+
+#endif
